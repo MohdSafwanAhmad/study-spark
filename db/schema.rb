ecb865cca8e4eea72a7aeb9963a272725fcb3b43
33919848+MohdSafwanAhmad@users.noreply.github.com <safwanahmad006@gmail.com>
2025-06-03 12:18:56 -0400

    feat: add updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,43 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_06_03_143058) do
+ActiveRecord::Schema[7.1].define(version: 2025_06_03_161324) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "expertises", force: :cascade do |t|
+    t.decimal "tutor_rate"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_expertises_on_user_id"
+  end
+
+  create_table "materials", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.string "format"
+    t.string "summary"
+    t.bigint "study_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["study_id"], name: "index_materials_on_study_id"
+  end
+
+  create_table "studies", force: :cascade do |t|
+    t.string "learning_objective"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_studies_on_user_id"
+  end
+
+  create_table "subjects", force: :cascade do |t|
+    t.string "name"
+    t.string "grade_level"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -22,8 +56,16 @@ ActiveRecord::Schema[7.1].define(version: 2025_06_03_143058) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.boolean "tutor"
+    t.datetime "date_of_birth"
+    t.string "bio"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "expertises", "users"
+  add_foreign_key "materials", "studies"
+  add_foreign_key "studies", "users"
 end

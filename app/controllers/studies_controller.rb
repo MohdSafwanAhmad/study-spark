da24054a1af16962130224600c380b39e7cfb023
exec zsh <diopmukhtar@hotmail.com>
2025-06-04 14:54:28 -0400

    learner_create_upload

---
+++ app/controllers/studies_controller.rb
@@ -0,0 +1,26 @@
+class StudiesController < ApplicationController
+  def index
+    @studies = Study.all
+  end
+
+  def new
+    @study = Study.new(subject_id: params[:subject_id])
+  end
+
+  def create
+    @study = Study.new(study_params)
+    @study.user = current_user
+
+    if @study.save
+      redirect_to dashboard_path, notice: "Subject added to your studies!"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def study_params
+    params.require(:study).permit(:learning_objective, :subject_id, :grade_level)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,12 @@ Rails.application.routes.draw do
   resources :tutors, only: [:index, :show]
 
   root to: "pages#home"
+  resources :studies do
+    member do
+      get :upload_materials
+      patch :attach_materials
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.

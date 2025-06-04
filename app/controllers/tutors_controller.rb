00f14e57799e275edfcf7cc5301ebc6475fb27ff
Matthew Meakin <meakin.matt@gmail.com>
2025-06-04 10:26:27 -0500

    added colors and fonts, also have filter button now functioning in /tutors

--- app/controllers/tutors_controller.rb
+++ app/controllers/tutors_controller.rb
@@ -1,6 +1,21 @@
 class TutorsController < ApplicationController
   def index
     @tutors = User.tutors.joins(:expertises).distinct
+
+    if params[:subject_id].present? && params[:max_price].present?
+      @tutors = @tutors.joins(:expertises)
+        .where(expertises: { subject_id: params[:subject_id] })
+        .where('expertises.tutor_rate <= ?', params[:max_price])
+        .distinct
+    elsif params[:subject_id].present?
+      @tutors = @tutors.joins(:expertises)
+        .where(expertises: { subject_id: params[:subject_id] })
+        .distinct
+    elsif params[:max_price].present?
+      @tutors = @tutors.joins(:expertises)
+        .where('expertises.tutor_rate <= ?', params[:max_price])
+        .distinct
+    end
   end
 
   def show

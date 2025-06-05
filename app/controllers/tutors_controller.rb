a209af39a40a0c04ea2ab3a62e245e69db336d8e
Matthew Meakin <meakin.matt@gmail.com>
2025-06-05 14:07:37 -0500

    more detailed pricing on filter

--- app/controllers/tutors_controller.rb
+++ app/controllers/tutors_controller.rb
@@ -21,15 +21,15 @@ class TutorsController < ApplicationController
     if params[:sort] == "price_desc"
       @tutors = @tutors
         .joins(:expertises)
-        .select('users.*, MIN(expertises.tutor_rate) AS min_rate')
+        .select('users.*, MAX(expertises.tutor_rate) AS max_rate')
         .group('users.id')
-        .order('min_rate DESC')
+        .order('max_rate DESC')
     elsif params[:sort] == "price_asc"
       @tutors = @tutors
         .joins(:expertises)
-        .select('users.*, MIN(expertises.tutor_rate) AS min_rate')
+        .select('users.*, MAX(expertises.tutor_rate) AS max_rate')
         .group('users.id')
-        .order('min_rate ASC')
+        .order('max_rate ASC')
     end
   end
 

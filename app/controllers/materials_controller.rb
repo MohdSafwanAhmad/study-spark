4a55fc77eb2e5dc6a7683343f0082f6ca34a1e4c
Naika Estriplet <naika.c.3113@gmail.com>
2025-06-05 12:08:45 -0400

    added routes to add new materials to a study/subject, created a form to upload materials, created respected controller actions, made no changes to the db or the schema

--- app/controllers/materials_controller.rb
+++ app/controllers/materials_controller.rb
@@ -3,4 +3,26 @@ class MaterialsController < ApplicationController
     @study = Study.find(params[:study_id])
     @materials = @study.materials
   end
+
+  def new
+    @study = Study.find(params[:study_id])
+    @material = Material.new
+  end
+
+  def create
+    @material = Material.new(material_params)
+    @material.study = @study
+
+    if @material.save
+      redirect_to study_materials_path(@study), notice: "Material uploaded successfully!"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def material_params
+    params.require(:material).permit(:name, :description, :summary)
+  end
 end

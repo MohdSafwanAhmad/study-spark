1bf720337139c06dfedb163914c8d6464b657a77
33919848+MohdSafwanAhmad@users.noreply.github.com <safwanahmad006@gmail.com>
2025-06-06 12:55:50 -0400

    sign up page redirects to dashboard

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+
+  private
+  
+  def after_sign_up_path_for(resource)
+    dashboard_path
+  end
+
 end

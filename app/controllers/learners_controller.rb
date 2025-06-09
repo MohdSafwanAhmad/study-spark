f22be4bb225952db92228ddc989a0836572626a6
exec zsh <diopmukhtar@hotmail.com>
2025-06-09 14:31:32 -0400

    authenticate

--- app/controllers/learners_controller.rb
+++ app/controllers/learners_controller.rb
@@ -4,4 +4,12 @@ class LearnersController < ApplicationController
       .includes(study: [:user, :subject, :materials])
       .where(expertise_id: current_user.expertises.pluck(:id))
   end
+
+  def show
+    @learner = User.find(params[:id])
+
+    unless TutoringSession.joins(:study).where(expertise_id: current_user.expertise_ids, studies: { user_id: @learner.id }).exists?
+      redirect_to tutor_path(current_user), alert: "Access denied to this learner profile."
+    end
+  end
 end

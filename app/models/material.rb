0cca63c687b18bfd74f30ebdcb4bc93f134c0878
33919848+MohdSafwanAhmad@users.noreply.github.com <safwanahmad006@gmail.com>
2025-06-10 18:02:51 -0400

    fix: remove generate more and validation

--- app/models/material.rb
+++ app/models/material.rb
@@ -2,5 +2,4 @@ class Material < ApplicationRecord
   belongs_to :study
   attr_accessor :file
   has_many :flashcards, dependent: :destroy
-  validates :raw_content, presence: true, length: { maximum: 10000 }
 end

1169d21f5dae33af226cfcf941f29b8cb678895c
Matthew Meakin <meakin.matt@gmail.com>
2025-06-11 08:29:23 -0500

    fix: chat

--- app/models/study.rb
+++ app/models/study.rb
@@ -4,7 +4,7 @@ class Study < ApplicationRecord
   belongs_to :subject
   has_many :materials
   has_many :tutoring_sessions
-  has_many :chats
+  has_many :chats, dependent: :destroy
 
   def fake_percent_complete
     require 'zlib'

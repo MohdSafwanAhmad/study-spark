009e309a667d395dcc2153bab8c99629950992e0
33919848+MohdSafwanAhmad@users.noreply.github.com <safwanahmad006@gmail.com>
2025-06-12 13:13:54 -0400

    fix chat and js code

--- app/controllers/studies_controller.rb
+++ app/controllers/studies_controller.rb
@@ -12,7 +12,7 @@ class StudiesController < ApplicationController
   def show
     # @study is set by before_action
 
-    @materials = @study.materials
+    @materials = @study.materials.order(created_at: :desc)
     @assigned_tutor = @study.tutoring_sessions.last&.expertise&.user
     @upcoming_session = @study.tutoring_sessions.order(start_time: :desc).first
     if @assigned_tutor
